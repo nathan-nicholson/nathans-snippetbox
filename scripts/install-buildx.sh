5bb3fa3c28cc2c1411c16d0da8f06facb464928b
Nathan Nicholson <nathan.r.nicholson@gmail.com>
2024-10-04 08:32:56 -0500

    chore: parameterize arch in buildx install script
    Signed-off-by: Nathan Nicholson <nathan.r.nicholson@gmail.com>

--- scripts/install-buildx.sh
+++ scripts/install-buildx.sh
@@ -2,7 +2,8 @@
 
 # Install docker buildx for use with docker/colima
 
-ARCH=arm64 # change to 'amd64' for x86_64
+# arm64 For M1 Macs, amd64 for Intel Macs
+ARCH=$1
 VERSION=v0.17.1 # Check https://github.com/docker/buildx/releases for the latest version
 curl -LO https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.darwin-${ARCH}
 mkdir -p ~/.docker/cli-plugins
